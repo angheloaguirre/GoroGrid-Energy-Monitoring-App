4797c85dbfcc0f1f132463a36532bd6011ec19e4
Anghelo <angheloaguirre@hotmail.com>
2025-11-23 17:59:08 -0500

    .

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -135,7 +135,7 @@ export function Dashboard({ userName = "Usuario" }: DashboardProps) {
         },
       };
 
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://gorogrid-backend-ceembfhbdkfabahz.brazilsouth-01.azurewebsites.net/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
